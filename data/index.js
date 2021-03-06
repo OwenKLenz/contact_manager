10bef49d39fe5f3a8f252b26997af97853e6f9f8
Owen Lenz <owen.lenz@gmail.com>
2021-03-06 17:34:18 -0500

    a few tweaks

--- data/index.js
+++ data/index.js
@@ -1,6 +1,6 @@
+require('dotenv').config();
 const pgp = require('pg-promise')();
-const connectionString  = "postgresql://oklenz:ThisIsAPassword@localhost:5432/contacts";
-
+const connectionString  = `postgresql://oklenz:${process.env.POSTGRES_PASS}@localhost:5432/contacts`;
 const db = pgp(connectionString);
 
 module.exports = db;
